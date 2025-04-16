610c8885d1999b3498dae6c8bc8e59f2a84ccc4f
João Mourato <joaomouratocn@gmail.com>
2025-04-16 09:37:50 -0300

    primeiro commit

---
+++ src/main/java/br/com/devjmcn/backend_projeto_temperatura/BackendProjetoTemperaturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devjmcn.backend_projeto_temperatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjetoTemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjetoTemperaturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devjmcn/backend_projeto_temperatura/BackendProjetoTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devjmcn.backend_projeto_temperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjetoTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
